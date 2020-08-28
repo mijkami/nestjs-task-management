46d9460b302c22a10898e7204f10c25a5aa035dd
GAILLOT Dimitri <d.gaillot@ics-nice.com>
2020-08-28 10:51:16 +0200

    add delete by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Body, Post } from '@nestjs/common';
+import { Controller, Get, Body, Post, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,8 +12,14 @@ export class TasksController {
         return this.tasksService.getAllTasks();
     }   
 
+    @Get('/:id')
+    getTaskbyID(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
     }
+
 }
