20658346081c6ed35cd5964044a2fe8103e3a75a
GAILLOT Dimitri <d.gaillot@ics-nice.com>
2020-08-28 12:20:40 +0200

    add filter search

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Get, Body, Post, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Body, Post, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDTO : GetTasksFilterDto): Task[] {
+        if(Object.keys(filterDTO).length) {
+            return this.tasksService.GetTasksWithFilters(filterDTO);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
     }   
+
 
     @Get('/:id')
     getTaskbyID(@Param('id') id: string): Task {
